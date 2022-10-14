20dae540db640775c2fc3e8ceaac32d82c03b36d
adriano de abreu <adrianodabreu@gmail.com>
2022-10-14 17:34:35 -0300

    Adicionado a tela de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,90 @@
+import { Envelope, LockKey } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
 import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-gray-400">Hello World</h1>
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
 
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-100">
-        Enviar
-      </button>
-    </>
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça o Login e comece a usar
+        </Text>
+      </header>
+
+      <form className="flex flex-col items-stretch w-full max-w-sm mt-10 gap-4">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="email"
+              id="email"
+              placeholder="Digite seu e-mail"
+            />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <LockKey />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="password"
+              id="password"
+              placeholder="Digite sua senha"
+            />
+          </TextInput.Root>
+        </label>
+
+        <label
+          htmlFor="remember"
+          className="flex items-center gap-2 cursor-pointer"
+        >
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">
+            Lembrar de mim por 30 dias
+          </Text>
+        </label>
+
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
+        </Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <a href="">
+          <Text
+            className="text-gray-400 underline hover:text-gray-200"
+            size="sm"
+          >
+            Esqueceu sua senha?
+          </Text>
+        </a>
+
+        <a href="" className="text-gray-400 underline">
+          <Text
+            className="text-gray-400 underline hover:text-gray-200"
+            size="sm"
+          >
+            Não possui conta? Crie agora!
+          </Text>
+        </a>
+      </footer>
+    </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,27 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import { ButtonHTMLAttributes } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       className={clsx(
-        'py-4 p-x3 bg-cyan-500 rounded front-semibold  text-black text-sm w-full transition-colors hover:bg-cyan-100 focus:ring-2 ring-gray-100'
+        'py-3 px-4 bg-cyan-500 rounded font-semibold  text-black text-sm w-full transition-colors hover:bg-cyan-100 focus:ring-2 ring-gray-100',
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
